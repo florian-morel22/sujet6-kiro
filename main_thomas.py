a2fe8727f93a84339a36b535bbf0dba0dadca6e2
Thomas NIEL <nieh@zhaw.ch>
2023-11-07 20:50:47 +0100

    ain updated

--- main_thomas.py
+++ main_thomas.py
@@ -1,1 +1,18 @@
-ma
+import json
+
+from docplex.cp.model import *
+
+
+with open("./tiny.json") as f:
+    data = json.load(f)
+
+parameters = data["parameters"]
+jobs = data["jobs"]
+tasks = data["tasks"]
+
+
+def cplexsolve():
+    print("solve model")
+
+
+cplexsolve()
