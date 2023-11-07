abf2202e40d659747f156dad674d8650d370044d
Florian MOREL <florian.morel@ensta-paris.fr>
2023-11-07 21:41:59 +0100

    ouais

--- main_thomas.py
+++ main_thomas.py
@@ -3,21 +3,22 @@ import json
 from docplex.cp.model import *
 
 
-with open("./tiny.json") as f:
+with open("./sujet/tiny.json") as f:
     data = json.load(f)
 
 parameters = data["parameters"]
-jobs = data["jobs"]
+data_jobs = data["jobs"]
 data_tasks = data["tasks"]
 
 
-def task_i_with_machine_k(i,k):
+def task_i_with_machine_k(i, k):
     for task in data["tasks"]:
         if task["task"] == i:
             for machine in task["machines"]:
                 if machine["machine"] == k:
                     return 1
     return 0
+
 
 def cplexsolve():
     # MODEL
@@ -33,11 +34,22 @@ def cplexsolve():
         for i in range(len(data_tasks))
     ]
 
+    jobs = [
+        model.integer_var_dict(
+            ["B", "C"],
+            min=0,
+            name="job_" + str(i),
+        )
+        for i in range(len(data_jobs))
+    ]
+
     # CONSTRAINTS
 
     model.add(tasks[i]["B"] <= 10 for i in range(len(data_tasks)))
 
-    model.add(task_i_with_machine_k(i,tasks[i]["m"]) == 1 for i in range(len(data_tasks)))
+    model.add(
+        task_i_with_machine_k(i, tasks[i]["m"]) == 1 for i in range(len(data_tasks))
+    )
 
     # OBJECTIVE
 
